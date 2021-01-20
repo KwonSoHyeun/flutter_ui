0511425006c7c2a783ae72ffd8be1e7da3f0ff07
SoHyeun Kwon <>
2021-01-20 10:58:11 +0900

    다른 페이지로 이동

--- flutter_ui_application/lib/main.dart
+++ flutter_ui_application/lib/main.dart
@@ -1,62 +1,91 @@
 import 'package:flutter/material.dart';
+import 'src/route/secondpage.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // 매트리얼앱을 생성하여 반환
     return MaterialApp(
-        title: 'Flutter Layout Demo',
-        theme: ThemeData(
-          // 앱의 테마 설정
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("Flutter layout demo"),
-            ),
-            body: listSection()));
-  }
-
-  Widget listSection() {
-    return ListView(children: <Widget>[
-      Container(
-          margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
-          child: Column(children: <Widget>[
-            Container(
-              height: 50,
-              decoration: BoxDecoration(color: Colors.amber),
-              child: Center(
-                child: Text(
-                  "Action1",
-                  style: TextStyle(
-                      color: Colors.blueGrey,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-          ])),
-      Container(
-          margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
-          child: Column(children: <Widget>[
-            Container(
-              height: 50,
-              decoration: BoxDecoration(color: Colors.amber),
-              child: Center(
-                child: Text(
-                  "Other",
-                  style: TextStyle(
-                      color: Colors.blueGrey,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-          ]))
-    ]);
+      title: 'Flutter Layout Demo',
+      theme: ThemeData(
+        // 앱의 테마 설정
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
   }
 }
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Flutter Firebase")),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text("Page 이동"),
+            subtitle: Text("다른 페이지로의 이동테스트입니다."),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => SecondRoute()));
+            },
+          ),
+          ListTile(
+            title: Text("Other menus"),
+            onTap: () {},
+          )
+        ].map((child) {
+          return Card(
+            child: child,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+// Scaffold(
+//     appBar: AppBar(
+//       title: Text("Flutter layout demo"),
+//     ),
+//     body: Column(children: <Widget>[
+//       Container(
+//         margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+//         child: RaisedButton(
+//           color: Colors.blueGrey[300],
+//           child: Text(
+//             "Cloud Firestore",
+//             style: TextStyle(color: Colors.white),
+//           ),
+//           onPressed: () {
+//             //Navigator.push(context,
+//             //   MaterialPageRoute(builder: (context) => SecondRoute()));
+
+//             Navigator.of(context).push(
+//                 MaterialPageRoute(builder: (context) => SecondRoute()));
+//           },
+//         ),
+//       ),
+//       Container(
+//           margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+//           child: Column(children: <Widget>[])),
+//       Container(
+//           margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+//           child: Column(children: <Widget>[
+//             Container(
+//               height: 50,
+//               decoration: BoxDecoration(color: Colors.amber),
+//               child: Center(
+//                 child: Text(
+//                   "Other",
+//                   style: TextStyle(
+//                       color: Colors.blueGrey,
+//                       fontSize: 18,
+//                       fontWeight: FontWeight.bold),
+//                 ),
+//               ),
+//             ),
+//           ]))
+//     ])));
---
+++ flutter_ui_application/lib/src/route/secondpage.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+// 두번째 라우트
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Layout Demo sub',
+        theme: ThemeData(
+          // 앱의 테마 설정
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Second Route'),
+          ),
+          body: Center(
+            // RiasedButton을 등록
+            child: RaisedButton(
+              child: Text('Go back!'),
+              onPressed: () {
+                // 버튼이 눌렸을 때 처리
+                Navigator.pop(context); // 현재 라우트를 pop하여 제거함
+              },
+            ),
+          ),
+        ));
+  }
+}
